95a1d379a68776292123f351554780e775b94261
romby <romby80@gmail.com>
2018-05-28 15:00:04 +0200

    Changes the files around a bit to reflect what main file is the primary.

--- main.cpp
+++ main.cpp
@@ -1,75 +1,100 @@
+#include <stdio.h>
 #include <unistd.h>   // Used for the sleep() and usleep() functions
 #include <iostream>
 #include <string>
 #include <thread>
-#include <mutex>
+// #include <mutex>
+
+using namespace std;
 
 // Using global variables so far...
-std::mutex msg_mutex;
-std::string msg;
+// mutex mut;
+// string * msg;
+// string msg;
 
-std::string kp_value = "110,000";
-std::string ki_value = "20,033";
-std::string kd_value = "32,000";
-std::string new_kp_value = kp_value;
-std::string new_ki_value = ki_value;
-std::string new_kd_value = kd_value;
+string kp_value = "10";
+string ki_value = "0.033";
+string kd_value = "2";
+string new_kp_value = kp_value;
+string new_ki_value = ki_value;
+string new_kd_value = kd_value;
+
+bool change = false;
 
 
-void ws_read()
+void ws_in()
 {
-  while (true)
+  while(true)
   {
-    std::string all = "";
-    std::string msg = "";
+    // mut.lock();
+
+    string msg = "";
     int id = 0;
-    
-    std::cin >> all;
 
-    id = stoi(all.substr(0,1));
-    msg = all.substr(1);
+    cin >> msg;
 
-    std::lock_guard<std::mutex> lock{msg_mutex};
+    id = stoi(msg.substr(0,1));    
 
     switch (id) {
-      case 1:
+      
+      case 1 :
         new_kp_value = msg;
         break;
-      case 2:
+      
+      case 2 :
         new_ki_value = msg;
         break;
-      case 3:
+      
+      case 3 :
         new_kd_value = msg;
         break;
+      
+      default:
+        break;
     }
+    
+    change = true;
+
+    // mut.unlock();
   }
 }
 
-void ws_write()
+void ws_out()
 {
-  while (true)
+  while(true)
   {
-    std::lock_guard<std::mutex> lock{msg_mutex};
+    // mut.lock();
 
-    if (kp_value != new_kp_value) { std::cout << new_kp_value << std::endl; usleep(500000); }
-    if (ki_value != new_ki_value) { std::cout << new_ki_value << std::endl; usleep(500000); }
-    if (kd_value != new_kd_value) { std::cout << new_kd_value << std::endl; usleep(500000); }
+    if(change)
+    {
+      if (kp_value.compare(new_kp_value) != 0) { cout << new_kp_value << endl; usleep(500000); }
+      if (ki_value.compare(new_ki_value) != 0) { cout << new_ki_value << endl; usleep(500000); }
+      if (kd_value.compare(new_kd_value) != 0) { cout << new_kd_value << endl; usleep(500000); }
+      change = false;
+    }
+
+    // mut.unlock();
+    usleep(500000);
   }
 }
 
 
 int main()
 {
-  sleep(5);
+  sleep(2);
+
+  // Disable input/output buffering.
+  setbuf(stdout, NULL);
+  setbuf(stdin, NULL);
 
   // Setting up initial values:
-  std::cout << kp_value << std::endl;
-  std::cout << ki_value << std::endl;
-  std::cout << kd_value << std::endl;
+  cout << '1' << kp_value << endl;
+  cout << '2' << ki_value << endl;
+  cout << '3' << kd_value << endl;
 
   // Creating two threads. One for reading and one for writing:
-  std::thread reader(ws_read);
-  std::thread writer(ws_write);
+  thread reader(ws_in);
+  thread writer(ws_out);
 
   reader.join();
   writer.join();
